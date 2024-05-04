4cdd1689d8126ffb1549ee143a4f08bef6548fd0
Mustanseer Sakerwala <connect.mustan@gmail.com>
2024-05-04 16:34:20 +0530

    fix: flutter based fixes

--- lib/linkrunner.dart
+++ lib/linkrunner.dart
@@ -1,17 +1,38 @@
+import 'dart:developer' as developer;
 import 'dart:convert';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:http/http.dart' as http;
 import 'package:device_info_plus/device_info_plus.dart';
 
+const packageName = "linkrunner";
+
+class UserData {
+  final String id;
+  final String? name;
+  final String? phone;
+  final String? email;
+
+  UserData({required this.id, required this.name, required this.phone, required this.email});
+
+  Map<String, String?> toJSON() {
+    return {
+      'id': id,
+      'name': name,
+      'phone': phone,
+      'email':email,
+    };
+  }
+}
+
 class LinkRunner {
   static const String baseUrl = 'https://api.linkrunner.io';
-  static const String encryptedStorageTokenName = 'linkrunner-token';
+  static const String encryptedStorageTokenKeyName = 'linkrunner-token';
   static const String packageVersion = '0.4.2';
 
-  final FlutterSecureStorage secureStorage = FlutterSecureStorage();
-  final DeviceInfoPlugin deviceInfo = DeviceInfoPlugin();
+  static const FlutterSecureStorage secureStorage = FlutterSecureStorage();
+  static final DeviceInfoPlugin deviceInfo = DeviceInfoPlugin();
 
-  Future<Map<String, dynamic>> getDeviceData() async {
+  static Future<Map<String, dynamic>> getDeviceData() async {
     var deviceData = <String, dynamic>{};
 
     try {
@@ -20,15 +41,15 @@ class LinkRunner {
       deviceData['package_version'] = packageVersion;
       deviceData['version'] = deviceData['version']; // Assuming version is fetched here
     } catch (e) {
-      print('Failed to get device info: $e');
+      developer.log('Failed to get device info', error: e, name: packageName);
     }
 
     return deviceData;
   }
 
-  Future<void> init(String token) async {
+  static Future<void> init(String token) async {
     if (token.isEmpty) {
-      print('Linkrunner needs your project token to initialize!');
+      developer.log('Linkrunner needs your project token to initialize!', name: packageName);
       return;
     }
 
@@ -49,18 +70,28 @@ class LinkRunner {
 
       var result = jsonDecode(response.body);
       if (response.statusCode == 200 || response.statusCode == 201) {
-        await secureStorage.write(key: encryptedStorageTokenName, value: token);
-        print('Linkrunner initialised successfully 🔥');
+        await secureStorage.write(key: encryptedStorageTokenKeyName, value: token);
+        developer.log('Linkrunner initialised successfully 🔥', name: packageName);
       } else {
         throw Exception(result['msg']);
       }
     } catch (e) {
-      print('Error initializing Linkrunner: $e');
+      developer.log('Error initializing Linkrunner', error: e, name: packageName);
     }
   }
 
-  Future<void> trigger(Map<String, dynamic> userData, Map<String, dynamic> data) async {
-    String? token = await secureStorage.read(key: encryptedStorageTokenName);
+  static Future<void> trigger(
+      {required UserData userData, Map<String, dynamic>? data}) async {
+    String? token = await secureStorage.read(key: encryptedStorageTokenKeyName);
+
+    final body = jsonEncode({
+      'token': token,
+      'user_data': userData.toJSON(),
+      'data': {
+        ...?data,
+        'device_data': await getDeviceData(),
+      },
+    });
 
     try {
       var response = await http.post(
@@ -69,26 +100,18 @@ class LinkRunner {
           'Accept': 'application/json',
           'Content-Type': 'application/json',
         },
-        body: jsonEncode({
-          'token': token,
-          'user_data': userData,
-          'data': {
-            ...data,
-            'device_data': await getDeviceData(),
-          },
-        }),
+        body: body,
       );
 
       var result = jsonDecode(response.body);
       if (response.statusCode == 200 || response.statusCode == 201) {
-        print('Linkrunner: Trigger called 🔥');
+        developer.log('Linkrunner: Trigger called 🔥', name: packageName);
       } else {
-        print('Linkrunner: Trigger failed');
-        print('Linkrunner: ${result['msg']}');
+        developer.log('Linkrunner: Trigger failed', name: packageName, error: jsonEncode(result['msg']));
       }
     } catch (e) {
-      print('Linkrunner: Trigger failed');
-      print('Linkrunner: $e');
+      developer.log('Linkrunner: Trigger failed', name: packageName, error: e);
     }
   }
 }
+
